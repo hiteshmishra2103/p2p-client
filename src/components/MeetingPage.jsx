20bfd39b61e911e59859762bba3c4042127ff1fa
Hitesh mishra <mishrahitesh854@gmail.com>
2023-12-27 13:07:59 +0530

    third and sixth are same

--- src/components/MeetingPage.jsx
+++ src/components/MeetingPage.jsx
@@ -30,6 +30,7 @@ export function MeetingPage() {
 
   const copyRoomId = () => {
     navigator.clipboard.writeText(roomId);
+    const meetingLink = `https://p2prealtime.vercel.app/meeting/meet/${roomId}`;
     Store.addNotification({
       title: "Meeting Link copied to clipboard",
       message: `Share this link with others to join the meeting`,
@@ -45,7 +46,7 @@ export function MeetingPage() {
 
   useEffect(() => {
     const s = socketIO.connect("https://p2pserver-zrex.onrender.com");
-    // const s = socketIO.connect("http://localhost:3001");
+
     s.on("callEnded", async () => {
       // Stop all tracks
       pc.close();
@@ -77,7 +78,6 @@ export function MeetingPage() {
           audio: true,
         })
         .then(async (stream) => {
-          stream.getTracks().forEach((track) => pc.addTrack(track));
           setVideoStream(stream);
         });
 
@@ -154,6 +154,7 @@ export function MeetingPage() {
                 pc.onicecandidate = ({ candidate }) => {
                   socket.emit("iceCandidate", { candidate });
                 };
+                pc.addTrack(videoStream.getVideoTracks()[0]);
                 try {
                   await pc.setLocalDescription(await pc.createOffer());
                   console.log({ aa: pc.localDescription });
